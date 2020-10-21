e42afde717d024310a8b3261a174cb976ee78927
IsmaielK <karim.ismaiel@vodafone.com>
2020-10-22 01:32:19 +0200

    - Created a controller to process root requests - Created a view to be rendered - Tested that the controller is returning what it must return

--- src/test/java/sia/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/sia/tacocloud/TacoCloudApplicationTests.java
@@ -1,8 +1,14 @@
 package sia.tacocloud;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.security.RunAs;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TacoCloudApplicationTests {
 
