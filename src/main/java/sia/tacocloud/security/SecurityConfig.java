ef3bbd5713cb247cd5494fa013cbcb10e2695701
IsmaielK <karim.ismaiel@vodafone.com>
2020-10-27 16:25:20 +0200

    Chapter 4 - JDBC-Based authentication

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -1,22 +1,29 @@
 package sia.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("kizo")
-                .password("kizo")
-                .roles("ROLE_USER")
-                .and()
-                .withUser("baba")
-                .password("baba")
-                .roles("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password FROM Users" +
+                        "WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, authority FROM UserAuthorities" +
+                        "WHERE username=?")
+                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
     }
 }
