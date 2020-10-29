94b936904ce8a3f03ff490e8215108736d707302
IsmaielK <karim.ismaiel@vodafone.com>
2020-10-29 16:44:29 +0200

    Chapter 4 - Configured LDAP security configuration

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -18,12 +18,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT username, password FROM Users" +
-                        "WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT username, authority FROM UserAuthorities" +
-                        "WHERE username=?")
-                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("uid={0}")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .contextSource()
+                .root("dc=tacocloud, dc=com")
+                .ldif("classpath:users.ldif")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("passcode");
+
     }
 }
